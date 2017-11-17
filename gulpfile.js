3194008441dc85618f0b6dbdb4c9a302c2a7ba39
Pataco80 <pataco80@gmail.com>
2017-11-17 19:54:02 +0100

    Création de toutes des pages portfolio, contact, et leurs includes. Fin session 73

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,53 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    include = require('gulp-file-include'),
+    clean = require('gulp-clean'),
     browserSync = require('browser-sync');
 
 // TASKS
+// Clean
+gulp.task('clean', function(){
+  return gulp.src('dist')
+    .pipe(clean());
+})
+
+// Copy
+gulp.task('copy', ['clean'], function(){
+  gulp.src([
+    'src/components/bootstrap/dist/**/*',
+    'src/components/bootstrap/fonts/**/*',
+    'src/components/bootstrap/js/**/*',
+    'src/components/font-awesome/css/**/*',
+    'src/components/font-awesome/fonts/**/*',
+    'src/css/**/*',
+    'src/js/**/*',
+    'src/img/**/*'
+  ], {"base": "src"})
+    .pipe(gulp.dest('dist'));
+})
+
 // Sass
 gulp.task('sass', function(){
   gulp.src('./src/scss/**/*.scss')
     .pipe(sass())
-  .pipe(gulp.dest('./src/css/'));
+  .pipe(gulp.dest('./dist/css/'));
 })
 
 // html
 gulp.task('html', function(){
-  gulp.src('./src/**/*.html');
+  return gulp.src('./src/*.html')
+    .pipe(include())
+    .pipe(gulp.dest('./dist/'));
 })
 
 // Server
-gulp.task('serve', function(){
+gulp.task('serve', ['html'], function(){
   browserSync.init({
     server: {
-      baseDir: 'src'
+      baseDir: 'dist'
     }
   })
-  gulp.watch('./src/**/*').on('change', browserSync.reload)
+  gulp.watch('./dist/**/*').on('change', browserSync.reload)
+  gulp.watch('./src/**/*.html', ['html'])
   gulp.watch('./src/scss/**/*.scss', ['sass'])
 });
